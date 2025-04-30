dcc6b66dbaf9e9c1c699985996923bc3739550ca
Naresh <Naresh@SQL-7SERVER>
2025-04-30 11:17:35 +0100

    New class created

---
+++ DDT_FrameWork/src/test/java/commonFunction/Demo.java
@@ -0,0 +1,17 @@
+package commonFunction;
+
+public class Demo {
+	public void add()
+	{
+		System.out.println("Addition");
+	}
+	public void div()
+	{
+		System.out.println("Division");
+}
+	public void mul()
+	{
+		System.out.println("Multiplication");
+	}
+
+}
